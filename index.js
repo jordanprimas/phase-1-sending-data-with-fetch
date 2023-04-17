2cac75a8d462ddc5b127a703fb6c5db8b652b6af
Jordan Primas <jorprimas20@gmail.com>
2023-04-16 17:04:45 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData (firstName, email) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+    })
+};
+return fetch("http://localhost:3000/users", configObj)
+.then(res => res.json())
+.then(body => addNewContact(body.id))
+
+.catch(errorFunc)
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName("body") [0].innerHTML = newID
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message
+}
